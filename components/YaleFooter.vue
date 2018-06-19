906eb54f7b396ccb5cd94ac2a76acbac15a7b6a4
Jake <jake101@gmail.com>
2018-06-19 02:21:04 -0700

    deploy button

--- components/YaleFooter.vue
+++ components/YaleFooter.vue
@@ -27,9 +27,11 @@
 </ul></div>
        </div>
              <div class="c-25 xs-text-left xs-p2 xs-border">
-               <div class="item"><div class="footer__heading xs-mb2">Contact</div>
+               <div class="item"><div class="footer__heading xs-mb2">Deploy</div>
                <ul class="list-unstyled">
-  <li>323.963.3240</li>
+  <li><a href="https://app.netlify.com/start/deploy?repository=https://github.com/jake-101/bael-template">
+  <img src="https://www.netlify.com/img/deploy/button.svg" title="Deploy to Netlify">
+</a></li>
 </ul></div>
        </div>
       <div class="c-12 xs-text-left xs-p2 xs-border">
