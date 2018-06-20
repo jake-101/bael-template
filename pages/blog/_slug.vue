9bbacc9224732ce3919b94719b002091dc236740
Jake <jake101@gmail.com>
2018-06-19 22:59:04 -0700

    blog post changes

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -5,10 +5,10 @@
 <div class="full-height single xs-border-left xs-border-right" :style="`min-height:calc(100vh - ${navbarheight}px);;margin-top:${navbarheight}px`">
   <div class=" xs-p2 bcg-item"> 
     <div class="item xs-block xs-full-height">
-       <div v-if="thumbnail"><img :src="thumbnail" :alt="title"></div>
-  <h1 class="xs-py3">{{title}}</h1>
+       <div v-if="thumbnail"><img class="featured-image" :src="thumbnail" :alt="title"></div>
+  <h1 class="xs-py3 main-title">{{title}}</h1>
    <div class="xs-mt-5 bold">{{ date | moment('dddd MMMM Do, YYYY') }}</div>
-     <div class="xs-py3"><div v-html="$md.render(body)"></div></div>
+     <div class="xs-py3 post-content"><div v-html="$md.render(body)"></div></div>
         </div>
         </div>
 
@@ -53,6 +53,11 @@ export default {
 </script>
 
 <style>
+.main-title {font-size: 3rem;}
+.featured-image {max-height:55vh;object-fit:cover;object-position: 50% 50%;width:100%;}
+.container .single .post-content {line-height: 1.618;color:#222;}
+.single .post-content p, .single .post-content li {margin-bottom:1rem;}
+.post-content h1,.post-content h2,.post-content h3,.post-content h4,.post-content h5,.post-content h6 {color:#000;margin-bottom:.5rem;} 
 .single {max-width:1200px;margin: 0 auto;}
 .search:focus {outline:none;}
 .footer__heading {text-transform:uppercase;}
